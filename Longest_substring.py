d33d98e44f8fd27eaacf168e5e0c898c8a7ce5b6
Nirupam Gangurde <90169731+nirupamgangurde@users.noreply.github.com>
2024-06-02 23:11:36 +0530

    Add files via upload

---
+++ Longest_substring.py
@@ -0,0 +1,14 @@
+def longest_unique_substring(s:str) -> int:
+    char_set = set()
+    left = 0
+    max_length = 0
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_length = max(max_length, right - left + 1)
+    return max_length
+print(longest_unique_substring("abcabcbb"))
+print(longest_unique_substring("bbbbb"))
+print(longest_unique_substring("pwwkew"))

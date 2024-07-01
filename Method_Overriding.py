eb461be3998cfc566dc2fe027dd60f97566e5029
Nirupam Gangurde <90169731+nirupamgangurde@users.noreply.github.com>
2024-07-01 17:37:53 +0530

    Add files via upload

---
+++ Method_Overriding.py
@@ -0,0 +1,21 @@
+class shape:
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+
+    def area(self):
+        return self.x*self.y
+
+class Circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+        super().__init__(radius,radius)
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+    def area(self):
+        return 3.14 * super().area()
+
+c = Circle(5)
+print(c.area())

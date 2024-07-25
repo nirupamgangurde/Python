528e8122c828560fa8a3237fa74d1c04c6d016b2
Nirupam Gangurde <90169731+nirupamgangurde@users.noreply.github.com>
2024-07-25 22:18:13 +0530

    Add files via upload

---
+++ 100 year age.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+years_until_100 = 100 - age
+print(f"Hello, {name}!")
+print(f"You will turn 100 years old in {years_until_100} years.")
